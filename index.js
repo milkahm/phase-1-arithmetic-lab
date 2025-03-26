9e3c02df149886a75aebcac6c1709875372a4ba9
milly <milkah.maina@student.moringaschool.com>
2025-03-26 15:15:59 +0300

    created index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+let multiply = num1 * num2
+
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 100) + 1; 
+
+console.log(random);
+
+let num4 = 10
+let num3 = 4 + (num4 * 1);
+
+let mod = num3 % num4;
+
+console.log(mod);
+
+let max = Math.max(5,12,15,20,18)
+
+console.log(max)
